2bcdfad311a4b4f7b0ccda593aa821620494b6fe
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-12 20:56:56 +0700

    learn reducer

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import store from './store'
 
 class App extends Component {
   componentWillMount() {
-    store.dispatch({ type: "INC", payload: 1})
-    store.dispatch({ type: "INC", payload: 1})
-    store.dispatch({ type: "INC", payload: 1})
+    store.dispatch({ type: "CHANGE_NAME", payload: "Rahman"})
+    store.dispatch({ type: "CHANGE_AGE", payload: 14})
+    store.dispatch({ type: "CHANGE_AGE", payload: 16})
   }
   render() {
     return (
--- src/store.js
+++ src/store.js
@@ -1,18 +1,42 @@
-import { createStore } from 'redux'
+import { combineReducers, createStore } from 'redux'
 
-const reducer = (state, action) => {
-  switch(action.type){
-    case "INC":
-      return state+action.payload
+// const reducer = (state, action) => {
+//   switch(action.type){
+//     case "INC":
+//       return state+action.payload
+//       break;
+//     case "DEC":
+//       return state-action.payload
+//       break;
+//   }
+//   return state;
+// }
+
+const userReducer = (state={}, action) => {
+  let new_state = {...state}
+  switch(action.type) {
+    case "CHANGE_NAME": {
+      new_state.name = action.payload;
       break;
-    case "DEC":
-      return state-action.payload
+    }
+    case "CHANGE_AGE": {
+      new_state.age = action.payload;
       break;
+    }
   }
-  return state;
+  return new_state
 }
 
-const store = createStore(reducer, 0)
+const tweetsReducer = (state=[], action) => {
+  return state
+}
+
+const reducers = combineReducers({
+  user: userReducer,
+  tweets: tweetsReducer
+})
+
+const store = createStore(reducers)
 
 store.subscribe(() => {
   console.log("store is change", store.getState());
