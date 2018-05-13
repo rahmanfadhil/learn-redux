24836b348d6a78c93737bbb09f92465a1c80e350
Rahman Fadhil <rhmnfadhil@gmail.com>
2018-05-13 09:57:41 +0700

    connect to react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import store from './store'
+// import store from './store'
 import axios from 'axios';
+import { connect } from 'react-redux'
 
+@connect((state) => {
+  return {
+    user: state.user
+  }
+})
 class App extends Component {
   componentWillMount() {
-    store.dispatch({
+    this.props.dispatch({
       type: "FETCH_USER",
       payload: axios.get('https://api.github.com/users/rahmanfadhil')
     })
   }
   render() {
+    console.log(this.props);
     return (
       <div>
         <h1>Hello World</h1>
